a233fcd26f317dec8e36a3997a642ff07751755f
Alix Field <32112823+kiaraBot@users.noreply.github.com>
2018-12-12 16:29:12 -0700

    Hubs

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,42 @@
+﻿// EverythingPizza - Group Project
+// CIS-2284 - November/December 2018
+// Group Members: Alix Field, Sharon Goodrich & Jonathan McPeak
+// Page: Hubs/ChatHub.cs
+
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// Sharon Goodrich sgoodrich1@cnm.edu
+
+namespace EverythingPizza.Hubs
+{   
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        //public Task SendMessageToGroup(string groupName, string message)
+        //{
+        //    return Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId}: {message}");
+        //}
+
+        //public async Task AddToGroup(string groupName)
+        //{
+        //    await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+
+        //    await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} has joined the group {groupName}.");
+        //}
+
+        //public async Task RemoveFromGroup(string groupName)
+        //{
+        //    await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+
+        //    await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} has left the group {groupName}.");
+        //}
+    }
+}
